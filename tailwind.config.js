b6c0ab0f08330cfd2d8305c655b412bc2b1c965b
Arun Rajak <arunrajak527@gmail.com>
2024-06-09 05:01:52 +0530

    Update tailwind.config.js
    fix resposiveness bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,13 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'sm': '440px',
-      // => @media (min-width: 640px) { ... }
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+      'lg': '1440px',
     },
     extend: {
       backgroundImage: {
